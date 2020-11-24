6bb6a75df69c39dad7a671bb5ba0603b064f42ad
delthas <delthas@dille.cc>
2020-11-24 17:54:01 +0100

    support adding links with a form

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
-//go:generate packer index.html index.go index
+//go:generate picopacker index.html index.go index
+//go:generate picopacker api.html api.go api
 package main
 
 import (
@@ -22,14 +23,62 @@ func randomHex(n int) string {
 	return hex.EncodeToString(bytes)[:n]
 }
 
+func add(w http.ResponseWriter, key string, url string, api bool) {
+	if len(key) == 0 || len(url) == 0 {
+		return
+	}
+
+	i := strings.Index(url, ":/")
+	if i >= 0 && len(url) > i+2 && url[i+2] != '/' {
+		url = url[:i] + "://" + url[i+2:]
+	}
+
+	if key == "hash" {
+		key = randomHex(8)
+	}
+
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("links"))
+		err := b.Put([]byte(key), []byte(url))
+		return err
+	})
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	var t *template.Template
+	if api {
+		t = apiTemplate
+	} else {
+		t = indexTemplate
+	}
+	t.Execute(w, struct {
+		Key string `html:"key"`
+		URL string `html:"url"`
+	}{
+		Key: urlPrefix + key,
+		URL: url,
+	})
+}
+
+var indexTemplate = template.Must(template.New("index").Parse(string(index)))
+var apiTemplate = template.Must(template.New("api").Parse(string(api)))
+
+var port int
+var urlPrefix string
+
+var db *bolt.DB
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	port := flag.Int("port", 8080, "http server listen port")
-	urlPrefix := flag.String("url", "http://localhost:8080/", "http server external url prefix")
+	flag.IntVar(&port, "port", 8080, "http server listen port")
+	flag.StringVar(&urlPrefix, "url", "http://localhost:8080/", "http server external url prefix")
 	flag.Parse()
 
-	db, err := bolt.Open("goshort.db", 0600, nil)
+	var err error
+	db, err = bolt.Open("goshort.db", 0600, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -43,9 +92,17 @@ func main() {
 		return nil
 	})
 
-	okTemplate := template.Must(template.New("ok").Parse(string(index)))
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		if r.Method == "POST" {
+			key := r.FormValue("key")
+			url := r.FormValue("url")
+			if key == "" {
+				key = "hash"
+			}
+			add(w, key, url, false)
+			return
+		}
 		if r.Method != "GET" {
 			w.WriteHeader(400)
 			return
@@ -58,9 +115,17 @@ func main() {
 		if path[0] == '/' {
 			path = path[1:]
 		}
+		if path == "" {
+			indexTemplate.Execute(w, nil)
+			return
+		}
 
 		i := strings.IndexRune(path, '/')
-		if i < 0 || i == len(path)-1 {
+		if i >= 0 && i < len(path)-1 {
+			key := path[:i]
+			url := path[i+1:]
+			add(w, key, url, true)
+		} else {
 			key := path
 			if i > 0 {
 				key = path[:i]
@@ -91,43 +156,8 @@ func main() {
 			}
 			w.Header().Set("Location", url)
 			w.WriteHeader(302)
-		} else {
-			key := path[:i]
-			url := path[i+1:]
-
-			if len(key) == 0 || len(url) == 0 {
-				return
-			}
-
-			i := strings.Index(url, ":/")
-			if i >= 0 && len(url) > i+2 && url[i+2] != '/' {
-				url = url[:i] + "://" + url[i+2:]
-			}
-
-			if key == "hash" {
-				key = randomHex(8)
-			}
-
-			err := db.Update(func(tx *bolt.Tx) error {
-				b := tx.Bucket([]byte("links"))
-				err := b.Put([]byte(key), []byte(url))
-				return err
-			})
-			if err != nil {
-				w.WriteHeader(500)
-				return
-			}
-
-			okTemplate.Execute(w, struct {
-				Short string `html:"short"`
-				Url   string `html:"url"`
-			}{
-				Short: *urlPrefix + key,
-				Url:   url,
-			})
 		}
-
 	})
-	fmt.Println("listening on " + *urlPrefix)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
+	fmt.Println("listening on " + urlPrefix)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
 }
